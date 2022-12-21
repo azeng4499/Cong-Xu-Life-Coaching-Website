4f931e0a5de665aeeecf6b69faad49b63d1fa596
Aaron Zeng <aaronz2003@gmail.com>
2022-12-20 21:33:43 -0500

    fixed testimony page

--- src/components/testimony.js
+++ src/components/testimony.js
@@ -18,7 +18,7 @@ const Testimony = () => {
     person3: false,
     person4: false,
     person5: false,
-    person6: false,   
+    person6: false,
   });
   useEffect(() => {
     Aos.init({ duration: 1500 });
@@ -30,7 +30,7 @@ const Testimony = () => {
         changePerson("person2");
       } else if (people["person2"]) {
         changePerson("person3");
-      } else if (people["person4"]) {
+      } else if (people["person3"]) {
         changePerson("person4");
       } else if (people["person4"]) {
         changePerson("person5");
@@ -48,17 +48,59 @@ const Testimony = () => {
 
   const changePerson = (person) => {
     if (person === "person1") {
-      setPeople({ person1: true, person2: false, person3: false, person4: false , person5: false, person6: false });
+      setPeople({
+        person1: true,
+        person2: false,
+        person3: false,
+        person4: false,
+        person5: false,
+        person6: false,
+      });
     } else if (person === "person2") {
-      setPeople({ person1: false, person2: true, person3: false, person4: false , person5: false, person6: false  });
+      setPeople({
+        person1: false,
+        person2: true,
+        person3: false,
+        person4: false,
+        person5: false,
+        person6: false,
+      });
     } else if (person === "person3") {
-      setPeople({ person1: false, person2: false, person3: true, person4: false , person5: false, person6: false  });
+      setPeople({
+        person1: false,
+        person2: false,
+        person3: true,
+        person4: false,
+        person5: false,
+        person6: false,
+      });
     } else if (person === "person4") {
-      setPeople({ person1: false, person2: false, person3: false, person4: true , person5: false, person6: false  });
+      setPeople({
+        person1: false,
+        person2: false,
+        person3: false,
+        person4: true,
+        person5: false,
+        person6: false,
+      });
     } else if (person === "person5") {
-      setPeople({ person1: false, person2: false, person3: false, person4: false , person5: true, person6: false  });
+      setPeople({
+        person1: false,
+        person2: false,
+        person3: false,
+        person4: false,
+        person5: true,
+        person6: false,
+      });
     } else {
-      setPeople({ person1: false, person2: false, person3: false, person4: false , person5: false, person6: true  });
+      setPeople({
+        person1: false,
+        person2: false,
+        person3: false,
+        person4: false,
+        person5: false,
+        person6: true,
+      });
     }
   };
 
@@ -85,37 +127,42 @@ const Testimony = () => {
             <Title>What People Are Saying</Title>
           </TitleWrapper>
           <TestimonyContainer data-aos="fade-up" data-aos-once="true">
-            <div style={{ display: "flex" }}>
+            <div
+              style={{
+                display: "flex",
+              }}
+            >
               <RiDoubleQuotesL size={"2em"} style={{ color: Colors.white }} />
               <TestimonyText>
-                {
-                people.person1 ? (
+                {people.person1 ? (
                   <div>
-                    The work I did with Cong lifted what felt like a huge weight of 
-                    concern and wasted energy from my shoulders.  Cong's deep listening 
-                    and her reflecting back to me what I had shared gave me the opportunity 
-                    to see my situation from a different lens in my mind.  The clarity of 
-                    her reflection allowed me to release the unnecessary burden I had 
-                    placed on myself, to accept a situation without having to change it.  
-                    I feel so much happier and at peace with myself and others because of 
+                    The work I did with Cong lifted what felt like a huge weight
+                    of concern and wasted energy from my shoulders. Cong's deep
+                    listening and her reflecting back to me what I had shared
+                    gave me the opportunity to see my situation from a different
+                    lens in my mind. The clarity of her reflection allowed me to
+                    release the unnecessary burden I had placed on myself, to
+                    accept a situation without having to change it. I feel so
+                    much happier and at peace with myself and others because of
                     my time with Cong......Cong is a blessing!
                   </div>
                 ) : people.person2 ? (
                   <div>
-                    Through her excellent coaching skills, Cong gently led me to an inner path
-                     that has always been there, but that somehow I had lost sight of.  
-                     That's the amazing thing–we all have subconscious beliefs that can hold
-                      us back from becoming the best person we can possibly be.  Cong's
-                       coaching let me brush the cobwebs from my own mirror to see the hidden
-                        but ever-present and true potential within myself that is joyfully 
-                        within my reach.
+                    Through her excellent coaching skills, Cong gently led me to
+                    an inner path that has always been there, but that somehow I
+                    had lost sight of. That's the amazing thing–we all have
+                    subconscious beliefs that can hold us back from becoming the
+                    best person we can possibly be. Cong's coaching let me brush
+                    the cobwebs from my own mirror to see the hidden but
+                    ever-present and true potential within myself that is
+                    joyfully within my reach.
                   </div>
                 ) : people.person3 ? (
                   <div>
-                    Working with Cong has been a delightful enlightening experience. 
-                    She has helped me become more aware of my own internal processes, 
-                    clarify my overall wants and goals, and provided a sense of support 
-                    and accountability.
+                    Working with Cong has been a delightful enlightening
+                    experience. She has helped me become more aware of my own
+                    internal processes, clarify my overall wants and goals, and
+                    provided a sense of support and accountability.
                   </div>
                 ) : people.person4 ? (
                   <div>
@@ -158,8 +205,83 @@ const Testimony = () => {
                 }}
               />
             </div>
+            <div>
+              {people.person1 ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "20px",
+                  }}
+                >
+                  <TestimonyText>- Leigh Smiley</TestimonyText>
+                  <TestimonyPerson src={Testimony1}></TestimonyPerson>
+                </div>
+              ) : people.person2 ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "20px",
+                  }}
+                >
+                  <TestimonyText>- Caterina Maxwell</TestimonyText>
+                  <TestimonyPerson src={Testimony2}></TestimonyPerson>
+                </div>
+              ) : people.person3 ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "20px",
+                  }}
+                >
+                  <TestimonyText>- Anna Jordan</TestimonyText>
+                  <TestimonyPerson src={Testimony3}></TestimonyPerson>
+                </div>
+              ) : people.person4 ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "20px",
+                  }}
+                >
+                  <TestimonyText>- Barbara Wilkinson</TestimonyText>
+                  <TestimonyPerson src={Testimony4}></TestimonyPerson>
+                </div>
+              ) : people.person5 ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "20px",
+                  }}
+                >
+                  <TestimonyText>- Frank Puglies</TestimonyText>
+                  <TestimonyPerson src={Testimony5}></TestimonyPerson>
+                </div>
+              ) : (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "20px",
+                  }}
+                >
+                  <TestimonyText>- Xiao Li</TestimonyText>
+                  <TestimonyPerson src={Testimony6}></TestimonyPerson>
+                </div>
+              )}
+            </div>
           </TestimonyContainer>
-          <TestimonyToolbar data-aos="fade-up" data-aos-once="true">
+          {/* <TestimonyToolbar data-aos="fade-up" data-aos-once="true">
           <TestimonyPersonContainer>
               <TestimonyPerson
                 src={Testimony1}
@@ -220,7 +342,7 @@ const Testimony = () => {
               />
               <TestimonyPersonLabel>Xiao Li</TestimonyPersonLabel>
             </TestimonyPersonContainer>
-          </TestimonyToolbar>
+          </TestimonyToolbar> */}
         </TestimonyWrapper>
       </Page>
     </>
@@ -275,7 +397,7 @@ const Title = styled.div`
 
 const TestimonyText = styled.div`
   font-family: "Kdam Thmor Pro", sans-serif;
-  font-size: 1.5rem;
+  font-size: 1rem;
   max-width: 1000px;
   color: ${Colors.white};
   text-align: center;
@@ -299,13 +421,15 @@ const TestimonyContainer = styled.div`
   width: 1200px;
   padding: 20px;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
+  flex-direction: column;
   border: solid;
   border-radius: 40px;
   border-width: 4px;
   border-color: white;
   background: ${Colors.darkBlue};
+
   @media screen and (max-width: 1300px) {
     width: 600px;
     height: 400px;
@@ -318,64 +442,61 @@ const TestimonyContainer = styled.div`
 
   @media screen and (max-width: 550px) {
     width: 325px;
-    height: 750px;
+    height: 500px;
   }
 
   @media screen and (max-width: 375px) {
     width: 300px;
-    height: 750px;
+    height: 500px;
   }
 `;
 
-const TestimonyToolbar = styled.div`
-  margin-top: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+// const TestimonyToolbar = styled.div`
+//   margin-top: 30px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
-const TestimonyPersonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 0px 50px;
+// const TestimonyPersonContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   margin: 0px 50px;
 
-  @media screen and (max-width: 775px) {
-    margin: 0px 20px;
-  }
-  @media screen and (max-width: 375px) {
-    margin: 0px 5px;
-  }
-`;
+//   @media screen and (max-width: 775px) {
+//     margin: 0px 20px;
+//   }
+//   @media screen and (max-width: 375px) {
+//     margin: 0px 5px;
+//   }
+// `;
 
 const TestimonyPerson = styled.img`
-  height: 100px;
-  width: 100px;
+  height: 50px;
+  width: 50px;
   object-fit: cover;
   border-radius: 50%;
   object-position: center;
-  border: solid 5px;
-  border-color: ${({ highlighted }) =>
-    highlighted ? Colors.orange : Colors.white};
 
-  @media screen and (max-width: 550px) {
-    height: 80px;
-    width: 80px;
-  }
+  // @media screen and (max-width: 550px) {
+  //   height: 50px;
+  //   width: 50px;
+  // }
 `;
 
-const TestimonyPersonLabel = styled.div`
-  font-family: "Kdam Thmor Pro", sans-serif;
-  font-size: 1rem;
-  margin-top: 10px;
-  text-align: center;
-  color: ${Colors.white}}
+// const TestimonyPersonLabel = styled.div`
+//   font-family: "Kdam Thmor Pro", sans-serif;
+//   font-size: 1rem;
+//   margin-top: 10px;
+//   text-align: center;
+//   color: ${Colors.white}}
 
-  @media screen and (max-width: 550px) {
-    font-size: 0.8rem;
-  }
-`;
+//   @media screen and (max-width: 550px) {
+//     font-size: 0.8rem;
+//   }
+// `;
 
 const Header = styled.div`
   color: ${Colors.white};
